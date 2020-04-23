4afb16bdf0c9274b88e4c9a78d90d828d565825b
Cindy Lee <cindylee0113@gmail.com>
2020-04-23 11:28:01 -0400

    Centering the constellations to be in the middle of the screen

--- constellation.js
+++ constellation.js
@@ -2,6 +2,7 @@ class Constellation{
   constructor(name, coords, lines){
     this.coords = coords;
     this.lines = lines;
+    this.translate();
     this.stars = [];
     for (let c = 0; c < this.coords.length; c++){
       this.stars[c] = new colorStar(coords[c][0], coords[c][1], 0);
@@ -51,5 +52,39 @@ class Constellation{
   
   is_done(){
     return this.stars[0].isDone();
+  }
+  
+  translate(){
+    const [centerX, centerY] = this.getCenter();
+    const diffX = width/2 - centerX;
+    const diffY = height/2- centerY;
+    for (let c = 0; c < this.coords.length; c++){
+      this.coords[c][0] += diffX;
+      this.coords[c][1] += diffY;
+    }
+    
+    for (let paths = 0; paths < this.lines.length; paths++){
+      let coordIndex = 0;
+      while (coordIndex < this.lines[paths].length){
+        this.lines[paths][coordIndex] += diffX;
+        this.lines[paths][coordIndex + 1] += diffY;
+        coordIndex += 2;
+      }
+    }
+    
+  }
+  
+  getCenter(){
+    let minX = this.coords[0][0];
+    let maxX = this.coords[0][0];
+    let minY = this.coords[0][1];
+    let maxY = this.coords[0][1];
+    for (let c = 1; c < this.coords.length; c++){
+      minX = min(this.coords[c][0], minX);
+      maxX = max(this.coords[c][0], maxX);
+      minY = min(this.coords[c][1], minY);
+      maxY = max(this.coords[c][1], maxY);
+    }
+    return [(minX + maxX)/2 , (minY + maxY) / 2];
   }
 }
